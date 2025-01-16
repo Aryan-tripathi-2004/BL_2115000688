2a17549eba0bc13e41befc8bad4f4716ab436d81
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-01-16 15:40:17 +0530

    Added : a while loop is added

--- SwitchCase.java
+++ SwitchCase.java
@@ -9,26 +9,30 @@ class SwitchCase{
         System.out.println("press 2 for Subtraction");
         System.out.println("press 3 for Multiplication");
         System.out.println("press 4 for Divide");
+        System.out.println("press 5 to exit from the loop");
 
         int Operation = sc.nextInt();
 
         sc.close();
 
-        switch(Operation){
-            case 1:
-                System.out.println("Sum of two numbers are :"+(num1+num2));
-                break;
-            case 2:
-                System.out.println("Subtraction of two numbers are :"+(num1-num2));
-                break;
-            case 3:
-                System.out.println("Multiplication of two numbers are :"+(num1*num2));
-                break;
-            case 4:
-                System.out.println("Division of two numbers are :"+(num1/num2));
-                break;
-            default:
-                System.out.println("Wrong Input..!!");
+        while(Operation!=5){
+            switch(Operation){
+                case 1:
+                    System.out.println("Sum of two numbers are :"+(num1+num2));
+                    break;
+                case 2:
+                    System.out.println("Subtraction of two numbers are :"+(num1-num2));
+                    break;
+                case 3:
+                    System.out.println("Multiplication of two numbers are :"+(num1*num2));
+                    break;
+                case 4:
+                    System.out.println("Division of two numbers are :"+(num1/num2));
+                    break;
+                default:
+                    System.out.println("Wrong Input..!!");
+                
+            }
         }
     }
 }
